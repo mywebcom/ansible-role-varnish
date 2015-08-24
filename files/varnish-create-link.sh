c89e3aa716a3e93ed27afa6c92d90928e064886a
Richard <richard@stuckonyou.com>
2015-08-24 14:14:15 +1000

    update

--- files/varnish-create-link.sh
+++ files/varnish-create-link.sh
@@ -3,14 +3,14 @@ if [ -f "/usr/bin/varnishd" ]
 then 
 	echo "installed";
 else
-	ln -s /usr/local/sbin/varnishd /usr/bin/varnishd;
-	ln -s /usr/local/bin/varnishtop /usr/bin/varnishtop;
-	ln -s /usr/local/bin/varnishhist /usr/bin/varnishhist;
-	ln -s /usr/local/bin/varnishtest /usr/bin/varnishtest;
-	ln -s /usr/local/bin/varnishncsa /usr/bin/varnishncsa;
-	ln -s /usr/local/bin/varnishlog /usr/bin/varnishlog;
-	ln -s /usr/local/bin/varnishstat /usr/bin/varnishstat;
-	ln -s /usr/local/bin/varnishsizes /usr/bin/varnishsizes;
-	ln -s /usr/local/bin/varnishreplay /usr/bin/varnishreplay;
-	ln -s /usr/local/bin/varnishadm /usr/bin/varnishadm;
+	ln -s /usr/local/varnish/sbin/varnishd /usr/bin/varnishd;
+	ln -s /usr/local/varnish/bin/varnishtop /usr/bin/varnishtop;
+	ln -s /usr/local/varnish/bin/varnishhist /usr/bin/varnishhist;
+	ln -s /usr/local/varnish/bin/varnishtest /usr/bin/varnishtest;
+	ln -s /usr/local/varnish/bin/varnishncsa /usr/bin/varnishncsa;
+	ln -s /usr/local/varnish/bin/varnishlog /usr/bin/varnishlog;
+	ln -s /usr/local/varnish/bin/varnishstat /usr/bin/varnishstat;
+	ln -s /usr/local/varnish/bin/varnishsizes /usr/bin/varnishsizes;
+	ln -s /usr/local/varnish/bin/varnishreplay /usr/bin/varnishreplay;
+	ln -s /usr/local/varnish/bin/varnishadm /usr/bin/varnishadm;
 fi;
